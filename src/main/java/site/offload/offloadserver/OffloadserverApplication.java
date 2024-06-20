7852bbb6329f54256b0b8cbf19efbf2785dcaf74
unanchoi <unanc2@gmail.com>
2024-06-21 02:18:00 +0900

    initial commit

---
+++ src/main/java/site/offload/offloadserver/OffloadserverApplication.java
@@ -0,0 +1,13 @@
+package site.offload.offloadserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffloadserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OffloadserverApplication.class, args);
+    }
+
+}
