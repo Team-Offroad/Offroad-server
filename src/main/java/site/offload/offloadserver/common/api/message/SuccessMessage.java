75cc6468e120590e34f7f09333428f818f356228
sansan20535 <20san20@daum.net>
2024-07-01 01:26:33 +0900

    feat : 공통 API Response 및 Message 생성

---
+++ src/main/java/site/offload/offloadserver/common/api/message/SuccessMessage.java
@@ -0,0 +1,11 @@
+package site.offload.offloadserver.common.api.message;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SuccessMessage {
+
+
+}
