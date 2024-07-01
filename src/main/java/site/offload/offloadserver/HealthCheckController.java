6f609c2b4855553250ba9667a4e572d2ff4c4690
sansan20535 <20san20@daum.net>
2024-07-01 16:29:58 +0900

    feat : Swagger 의존성 추가 및 테스트

--- src/main/java/site/offload/offloadserver/HealthCheckController.java
+++ src/main/java/site/offload/offloadserver/HealthCheckController.java
@@ -1,11 +1,12 @@
 package site.offload.offloadserver;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Tag(name = "HealthCheck API", description = "HealthCheck 관련 API")
 @RestController
-public class HealthCheckController {
+public class HealthCheckController implements HealthCheckControllerSwagger{
 
     @GetMapping("/api/health")
     public String healthCheck() {
