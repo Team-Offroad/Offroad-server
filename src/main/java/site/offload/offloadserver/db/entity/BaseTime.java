8131c5431de5ded2b690e1870ed9b8988ebf9850
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-07-07 02:29:58 +0900

    [refactor] 엔티티 생성시간, 수정시간 DB 제약 조건 추가

--- src/main/java/site/offload/offloadserver/db/entity/BaseTime.java
+++ src/main/java/site/offload/offloadserver/db/entity/BaseTime.java
@@ -1,5 +1,6 @@
 package site.offload.offloadserver.db.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -15,9 +16,11 @@ import java.time.LocalDateTime;
 public abstract class BaseTime {
 
     @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
 }
 
