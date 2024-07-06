05612876e53f3e233600e686a5128ced3ffcd5c3
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-07-07 02:22:20 +0900

    [feat] GainedCharacter 리포지토리 추가"

---
+++ src/main/java/site/offload/offloadserver/db/repository/GainedCharacterRepository.java
@@ -0,0 +1,7 @@
+package site.offload.offloadserver.db.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import site.offload.offloadserver.db.entity.GainedCharacter;
+
+public interface GainedCharacterRepository extends JpaRepository<GainedCharacter, Integer> {
+}
