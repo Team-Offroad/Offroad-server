d4682e69deb9deee4020f29e05d5b0a3fbc0270f
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-07-07 18:52:00 +0900

    [refactor] JpaRepository -> CrudRepository 변경

--- src/main/java/site/offload/offloadserver/db/repository/CharacterRepository.java
+++ src/main/java/site/offload/offloadserver/db/repository/CharacterRepository.java
@@ -1,7 +1,7 @@
 package site.offload.offloadserver.db.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import site.offload.offloadserver.db.entity.Character;
 
-public interface CharacterRepository extends JpaRepository<Character, Integer> {
+public interface CharacterRepository extends CrudRepository<Character, Integer> {
 }
