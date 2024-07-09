60ef65c372f844e99898cb6f7b190ba5c9357098
sansan20535 <20san20@daum.net>
2024-07-10 06:35:50 +0900

    feat : filter 설정

---
+++ src/main/java/site/offload/offloadserver/common/auth/filter/CustomJwtAuthenticationEntryPoint.java
@@ -0,0 +1,36 @@
+package site.offload.offloadserver.common.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import site.offload.offloadserver.api.message.ErrorMessage;
+import site.offload.offloadserver.api.response.APIErrorResponse;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomJwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        setResponse(response);
+    }
+
+    private void setResponse(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter()
+                .write(objectMapper.writeValueAsString(
+                        APIErrorResponse.of(HttpStatus.UNAUTHORIZED.value(), ErrorMessage.JWT_UNAUTHORIZED_EXCEPTION.getMessage())));
+    }
+}
