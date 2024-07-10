0cebdc33bb039b7c66e9b34ac8d54a1264eb42e4
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-07-10 16:17:00 +0900

    [fix] GlobalExceptionHandler 수정

--- src/main/java/site/offload/offloadserver/api/exception/GlobalExceptionHandler.java
+++ src/main/java/site/offload/offloadserver/api/exception/GlobalExceptionHandler.java
@@ -11,28 +11,28 @@ import site.offload.offloadserver.api.response.APIErrorResponse;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<APIErrorResponse> handleBadRequestException(final RuntimeException exception) {
-        return APIErrorResponse.of(HttpStatus.BAD_REQUEST.value(), exception.getMessage());
+    public ResponseEntity<APIErrorResponse> handleBadRequestException(final OffroadException exception) {
+        return APIErrorResponse.of(HttpStatus.BAD_REQUEST.value(), exception.getMessage(), exception.getCustomErrorCode());
     }
 
     @ExceptionHandler(UnAuthorizedException.class)
-    public ResponseEntity<APIErrorResponse> handleUnauthorizedException(final RuntimeException exception) {
-        return APIErrorResponse.of(HttpStatus.UNAUTHORIZED.value(), exception.getMessage());
+    public ResponseEntity<APIErrorResponse> handleUnauthorizedException(final OffroadException exception) {
+        return APIErrorResponse.of(HttpStatus.UNAUTHORIZED.value(), exception.getMessage(), exception.getCustomErrorCode());
     }
 
     @ExceptionHandler(ForbiddenException.class)
-    public ResponseEntity<APIErrorResponse> handleForbiddenException(final RuntimeException exception) {
-        return APIErrorResponse.of(HttpStatus.FORBIDDEN.value(), exception.getMessage());
+    public ResponseEntity<APIErrorResponse> handleForbiddenException(final OffroadException exception) {
+        return APIErrorResponse.of(HttpStatus.FORBIDDEN.value(), exception.getMessage(), exception.getCustomErrorCode());
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<APIErrorResponse> handleNotFoundException(final RuntimeException exception) {
-        return APIErrorResponse.of(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+    public ResponseEntity<APIErrorResponse> handleNotFoundException(final OffroadException exception) {
+        return APIErrorResponse.of(HttpStatus.NOT_FOUND.value(), exception.getMessage(), exception.getCustomErrorCode());
     }
 
     @ExceptionHandler(OffroadException.class)
     public ResponseEntity<APIErrorResponse> handleCustomException(final OffroadException exception) {
         Sentry.captureException(exception);
-        return APIErrorResponse.of(exception.getHttpStatus().value(), exception.getMessage(), exception.getCustomErrorCode());
+        return APIErrorResponse.of(exception.getHttpStatus().value(), exception.getMessage());
     }
 }
