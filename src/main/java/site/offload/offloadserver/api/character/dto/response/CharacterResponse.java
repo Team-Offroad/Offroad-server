b207214d2a04a7baeac0e8a60c233b9647a2ebb3
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-07-16 04:33:47 +0900

    [fix] CharacerResponse dto 스택오버플로우 오류

--- src/main/java/site/offload/offloadserver/api/character/dto/response/CharacterResponse.java
+++ src/main/java/site/offload/offloadserver/api/character/dto/response/CharacterResponse.java
@@ -2,15 +2,15 @@ package site.offload.offloadserver.api.character.dto.response;
 
 import lombok.Builder;
 
+@Builder
 public record CharacterResponse(Integer id, String description, String characterBaseImageUrl, String characterCode) {
 
-    @Builder
     public static CharacterResponse of(Integer id, String description, String characterBaseImageUrl, String characterCode) {
         return CharacterResponse.builder()
-                                .id(id)
-                                .description(description)
-                                .characterBaseImageUrl(characterBaseImageUrl)
-                                .characterCode(characterCode).build();
+                .id(id)
+                .description(description)
+                .characterBaseImageUrl(characterBaseImageUrl)
+                .characterCode(characterCode).build();
 
     }
 }
--- src/main/java/site/offload/offloadserver/db/character/repository/CharacterRepository.java
+++ src/main/java/site/offload/offloadserver/db/character/repository/CharacterRepository.java
@@ -1,7 +1,8 @@
 package site.offload.offloadserver.db.character.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import site.offload.offloadserver.db.character.entity.Character;
 
-public interface CharacterRepository extends CrudRepository<Character, Integer> {
+public interface CharacterRepository extends JpaRepository<Character, Integer> {
 }
