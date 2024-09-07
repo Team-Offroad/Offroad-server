b8fc622791e643a55d17e2b4011492db70cf25e4
unanchoi <unanc2@gmail.com>
2024-09-08 01:33:26 +0900

    fix: 장소 조회 API 응답데이터 수정

--- offroad-api/src/main/java/site/offload/api/place/dto/response/RegisteredPlaceResponse.java
+++ offroad-api/src/main/java/site/offload/api/place/dto/response/RegisteredPlaceResponse.java
@@ -1,6 +1,7 @@
 package site.offload.api.place.dto.response;
 
 
+import site.offload.enums.place.PlaceArea;
 import site.offload.enums.place.PlaceCategory;
 
 public record RegisteredPlaceResponse(
@@ -9,12 +10,34 @@ public record RegisteredPlaceResponse(
         String address,
         String shortIntroduction,
         PlaceCategory placeCategory,
+        PlaceArea placeArea,
         double latitude,
         double longitude,
         Long visitCount,
         String categoryImageUrl
 ) {
-    public static RegisteredPlaceResponse of(Long id, String name, String address, String shortIntroduction, PlaceCategory placeCategory, double latitude, double longitude, Long visitCount, String categoryImageUrl) {
-        return new RegisteredPlaceResponse(id, name, address, shortIntroduction, placeCategory, latitude, longitude, visitCount, categoryImageUrl);
+    public static RegisteredPlaceResponse of(
+            Long id,
+            String name,
+            String address,
+            String shortIntroduction,
+            PlaceCategory placeCategory,
+            PlaceArea placeArea,
+            double latitude,
+            double longitude,
+            Long visitCount,
+            String categoryImageUrl
+    ) {
+        return new RegisteredPlaceResponse(
+                id,
+                name,
+                address,
+                shortIntroduction,
+                placeCategory,
+                placeArea,
+                latitude,
+                longitude,
+                visitCount,
+                categoryImageUrl);
     }
 }
