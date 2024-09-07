011c2d8bbc7b3deea27997eadac7187cb7a0729f
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-09-08 01:44:19 +0900

    chore: 홈화면 모션캡쳐이미지 반환 추가

--- offroad-api/src/main/java/site/offload/api/member/dto/response/MemberAdventureInformationResponse.java
+++ offroad-api/src/main/java/site/offload/api/member/dto/response/MemberAdventureInformationResponse.java
@@ -5,16 +5,18 @@ import lombok.Builder;
 @Builder
 public record MemberAdventureInformationResponse(String nickname, String emblemName, String baseImageUrl,
                                                  String motionImageUrl,
-                                                 String characterName) {
+                                                 String characterName,
+                                                 String motionCaptureImageUrl) {
 
     public static MemberAdventureInformationResponse of(final String nickname, final String emblemName,
-                                                        final String baseImageUrl, final String motionImageUrl, final String characterName) {
+                                                        final String baseImageUrl, final String motionImageUrl, final String characterName, final String motionCaptureImageUrl) {
         return MemberAdventureInformationResponse.builder()
                 .nickname(nickname)
                 .emblemName(emblemName)
                 .characterName(characterName)
                 .baseImageUrl(baseImageUrl)
                 .motionImageUrl(motionImageUrl)
+                .motionCaptureImageUrl(motionCaptureImageUrl)
                 .build();
     }
 }
