383963664840b232d406917ff0142d5983b34a05
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-09-12 21:16:51 +0900

    chore: 쿠폰 테스트 코드 수정

--- offroad-api/src/test/java/site/offload/api/coupon/usecase/CouponApplyUseCaseTest.java
+++ offroad-api/src/test/java/site/offload/api/coupon/usecase/CouponApplyUseCaseTest.java
@@ -54,7 +54,7 @@ class CouponApplyUseCaseTest {
     @DisplayName("사용자는 획득하지 않은 쿠폰에 대한 쿠폰 사용 요청시 인증에 실패한다.")
     void failToApplyCouponWhenNotGainedCoupon() {
         //given
-        CouponApplyRequest couponApplyRequest = new CouponApplyRequest("testcode", 1L, 1L);
+        CouponApplyRequest couponApplyRequest = new CouponApplyRequest("testcode", 1L);
         given(gainedCouponService.isExistByMemberEntityIdAndCouponId(anyLong(), anyLong())).willReturn(false);
 
         //when
@@ -70,7 +70,7 @@ class CouponApplyUseCaseTest {
         //given
         MemberEntity memberEntity = createMemberEntity("sub", "text@test.com", SocialPlatform.GOOGLE, "kim");
         CouponEntity couponEntity = createCoupon("name", "url", "description", "couponCode");
-        CouponApplyRequest couponApplyRequest = new CouponApplyRequest("testcode", 1L, 1L);
+        CouponApplyRequest couponApplyRequest = new CouponApplyRequest("testcode", 1L);
         GainedCouponEntity gainedCouponEntity = createGainedCouponEntity(memberEntity, couponEntity);
 
         gainedCouponEntity.updateIsUsed(true);
@@ -85,29 +85,6 @@ class CouponApplyUseCaseTest {
         assertThat(actualResponse.success()).isFalse();
     }
 
-    @Test
-    @DisplayName("사용자는 유효하지 않은 쿠폰 사용 코드를 입력할 경우 쿠폰 사용 인증에 실패한다")
-    void failWhenInvalidCouponAuthCode() {
-        //given
-        MemberEntity memberEntity = createMemberEntity("sub", "text@test.com", SocialPlatform.GOOGLE, "kim");
-        CouponEntity couponEntity = createCoupon("name", "url", "description", "couponCode");
-        PlaceEntity placeEntity = createPlace(PlaceArea.AREA1, PlaceCategory.CULTURE, "테스트이름", "테스트주소",
-                "테스트소개", "1234", 1234.1234, 234.241, "테스트카테고리이미지URL", "테스트쿠폰사용코드");
-
-        // 유효하지 않은 쿠폰 코드 입력 -> invalidAuthCode
-        CouponApplyRequest couponApplyRequest = new CouponApplyRequest("invalidAuthCode", 1L, 1L);
-        GainedCouponEntity gainedCouponEntity = createGainedCouponEntity(memberEntity, couponEntity);
-
-        given(gainedCouponService.isExistByMemberEntityIdAndCouponId(anyLong(), anyLong())).willReturn(true);
-        given(gainedCouponService.findByMemberEntityIdAndCouponId(anyLong(), anyLong())).willReturn(gainedCouponEntity);
-        given(placeService.findPlaceById(anyLong())).willReturn(placeEntity);
-
-        //when
-        CouponApplyResponse actualResponse = couponApplyUseCase.applyCoupon(1L, couponApplyRequest);
-
-        //then
-        assertThat(actualResponse.success()).isFalse();
-    }
 
     @Test
     @DisplayName("사용자는 특정 장소 카테고리의 장소에서 쿠폰을 사용할 수 있다.")
@@ -116,7 +93,7 @@ class CouponApplyUseCaseTest {
         CouponEntity couponEntity = createCoupon("name", "url", "description", "couponCode");
         PlaceEntity placeEntity = createPlace(PlaceArea.AREA1, PlaceCategory.CAFFE, "테스트이름", "테스트주소",
                 "테스트소개", "1234", 1234.1234, 234.241, "테스트카테고리이미지URL", "테스트쿠폰사용코드");
-        CouponApplyRequest couponApplyRequest = new CouponApplyRequest("테스트쿠폰사용코드", 1L, 1L);
+        CouponApplyRequest couponApplyRequest = new CouponApplyRequest("테스트쿠폰사용코드", 1L);
         GainedCouponEntity gainedCouponEntity = createGainedCouponEntity(memberEntity, couponEntity);
         RewardList rewardList = new RewardList(false, "couponCode", "emblemCode", false);
         QuestRewardEntity questRewardEntity = createQuestReward(1, rewardList);
@@ -128,7 +105,6 @@ class CouponApplyUseCaseTest {
         given(questRewardService.findByCouponCode(anyString())).willReturn(questRewardEntity);
         given(placeService.findByCouponAuthCode(anyString())).willReturn(placeEntity);
         given(questService.findById(anyInt())).willReturn(questEntity);
-        given(placeService.findPlaceById(anyLong())).willReturn(placeEntity);
 
         //when
         CouponApplyResponse actualResponse = couponApplyUseCase.applyCoupon(1L, couponApplyRequest);
@@ -145,7 +121,7 @@ class CouponApplyUseCaseTest {
         CouponEntity couponEntity = createCoupon("name", "url", "description", "couponCode");
         PlaceEntity placeEntity = createPlace(PlaceArea.AREA1, PlaceCategory.RESTAURANT, "테스트이름", "테스트주소",
                 "테스트소개", "1234", 1234.1234, 234.241, "테스트카테고리이미지URL", "테스트쿠폰사용코드");
-        CouponApplyRequest couponApplyRequest = new CouponApplyRequest("테스트쿠폰사용코드", 1L, 1L);
+        CouponApplyRequest couponApplyRequest = new CouponApplyRequest("테스트쿠폰사용코드", 1L);
         GainedCouponEntity gainedCouponEntity = createGainedCouponEntity(memberEntity, couponEntity);
         RewardList rewardList = new RewardList(false, "couponCode", "emblemCode", false);
         QuestRewardEntity questRewardEntity = createQuestReward(1, rewardList);
@@ -157,7 +133,6 @@ class CouponApplyUseCaseTest {
         given(questRewardService.findByCouponCode(anyString())).willReturn(questRewardEntity);
         given(placeService.findByCouponAuthCode(anyString())).willReturn(placeEntity);
         given(questService.findById(anyInt())).willReturn(questEntity);
-        given(placeService.findPlaceById(anyLong())).willReturn(placeEntity);
 
         //when
         CouponApplyResponse actualResponse = couponApplyUseCase.applyCoupon(1L, couponApplyRequest);
@@ -173,7 +148,7 @@ class CouponApplyUseCaseTest {
         CouponEntity couponEntity = createCoupon("name", "url", "description", "couponCode");
         PlaceEntity placeEntity = createPlace(PlaceArea.NONE, PlaceCategory.NONE, "테스트이름", "테스트주소",
                 "테스트소개", "1234", 1234.1234, 234.241, "테스트카테고리이미지URL", "테스트쿠폰사용코드");
-        CouponApplyRequest couponApplyRequest = new CouponApplyRequest("테스트쿠폰사용코드", 1L, 1L);
+        CouponApplyRequest couponApplyRequest = new CouponApplyRequest("테스트쿠폰사용코드", 1L);
         GainedCouponEntity gainedCouponEntity = createGainedCouponEntity(memberEntity, couponEntity);
         RewardList rewardList = new RewardList(false, "couponCode", "emblemCode", false);
         QuestRewardEntity questRewardEntity = createQuestReward(1, rewardList);
@@ -187,7 +162,6 @@ class CouponApplyUseCaseTest {
         given(questRewardService.findByCouponCode(anyString())).willReturn(questRewardEntity);
         given(placeService.findByCouponAuthCode(anyString())).willReturn(placeEntity);
         given(questService.findById(anyInt())).willReturn(questEntity);
-        given(placeService.findPlaceById(anyLong())).willReturn(placeEntity);
 
         //when
         CouponApplyResponse actualResponse = couponApplyUseCase.applyCoupon(1L, couponApplyRequest);
