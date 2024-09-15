e328a8266ed990ba025b373cdc92d1e7031703fc
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-09-16 00:57:44 +0900

    refactor: swagger 문서 정보 추가

--- offroad-api/src/main/java/site/offload/api/member/controller/MemberControllerSwagger.java
+++ offroad-api/src/main/java/site/offload/api/member/controller/MemberControllerSwagger.java
@@ -1,7 +1,10 @@
 package site.offload.api.member.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -21,8 +24,20 @@ public interface MemberControllerSwagger {
     @Operation(summary = "모험 정보 인증 API", description = "메인 홈에서 모험 인증 정보(닉네임, 캐릭터 이미지 or 모션 이미지, 칭호)반환")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "모험 인증 정보 요청 성공"),
-            @ApiResponse(responseCode = "400", description = "모험 인증 정보 요청 실패", content = @Content(schema = @Schema(implementation = APIErrorResponse.class)))
+            @ApiResponse(responseCode = "400", description = "모험 인증 정보 요청 실패, 사용자가 획득한 캐릭터가 아닙니다.", content = @Content(schema = @Schema(implementation = APIErrorResponse.class),
+                    examples = {
+                            @ExampleObject(name = "사용자가 획득하지 못한 캐릭터", value = "{ \"message\": \"사용자가 획득한 캐릭터가 아닙니다\", \"customErrorCode\": \"NOT_GAINED_CHARACTER\" }"),
+                    })),
+            @ApiResponse(responseCode = "404", description = "요청에 필요한 자원이 없음", content = @Content(schema = @Schema(implementation = APIErrorResponse.class),
+                    examples = {
+                            @ExampleObject(name = "존재하지 않는 유저", value = "{ \"message\": \"존재하지 않는 유저\", \"customErrorCode\": \"NOT_EXISTS_MEMBER\" }"),
+                            @ExampleObject(name = "존재하지 않는 캐릭터", value = "{ \"message\": \"존재하지 않는 캐릭터\", \"customErrorCode\": \"NOT_EXISTS_CHARACTER\" }"),
+                            @ExampleObject(name = "존재하지 않는 장소 카테고리", value = "{ \"message\": \"존재하지 않는 장소 카테고리\", \"customErrorCode\": \"NOT_EXISTS_PLACE_CATEGORY\" }"),
+                            @ExampleObject(name = "존재하지 않는 캐릭터 모션", value = "{ \"message\": \"존재하지 않는 캐릭터 모션\", \"customErrorCode\": \"NOT_EXISTS_CHARACTER_MOTION\" }"),
+                    }))
     })
+    @Parameter(name = "Authorization", description = "Bearer {access_token}", in = ParameterIn.HEADER, required = true, schema = @Schema(type = "string"))
+    @Parameter(name = "category", description = "장소 카테고리", in = ParameterIn.QUERY, required = true, schema = @Schema(type = "string"))
     ResponseEntity<APISuccessResponse<MemberAdventureInformationResponse>> getAdventureInformation(@RequestParam(value = "category") final String category);
 
     @Operation(summary = "프로필 업데이트 API", description = "프로필 업데이트 정보를 받아 멤버 업데이트")
@@ -55,8 +70,21 @@ public interface MemberControllerSwagger {
     ResponseEntity<APISuccessResponse<VerifyQrcodeResponse>> authAdventure(final @RequestBody AuthAdventureRequest request);
 
 
-    @Operation(summary = "위치 정보 탐험 인증 API", description = "QR코드로 탐험인증 하는 API")
-    @ApiResponse(responseCode = "200", description = "탐험 인증 성공")
+    @Operation(summary = "위치 정보 탐험 인증 API", description = "위치정보로 탐험인증 하는 API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "탐험 인증 성공"),
+            @ApiResponse(responseCode = "404", description = "요청에 필요한 자원 없음",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = APIErrorResponse.class),
+                            examples = {
+                                    @ExampleObject(name = "존재하지 않는 장소", value = "{ \"message\": \"존재하지 않는 장소\", \"customErrorCode\": \"NOT_EXISTS_PLACE\" }"),
+                                    @ExampleObject(name = "존재하지 않는 캐릭터", value = "{ \"message\": \"존재하지 않는 캐릭터\", \"customErrorCode\": \"NOT_EXISTS_CHARACTER\" }"),
+                                    @ExampleObject(name = "존재하지 않는 유저", value = "{ \"message\": \"존재하지 않는 유저\", \"customErrorCode\": \"NOT_EXISTS_MEMBER\" }"),
+                                    @ExampleObject(name = "존재하지 않는 퀘스트", value = "{ \"message\": \"존재하지 않는 퀘스트\", \"customErrorCode\": \"NOT_EXISTS_QUEST\" }")
+                            }
+                    ))
+    })
+    @Parameter(name = "Authorization", description = "Bearer {access_token}", in = ParameterIn.HEADER, required = true, schema = @Schema(type = "string"))
     ResponseEntity<APISuccessResponse<VerifyPositionDistanceResponse>> authAdventureOnlyPlace(final @RequestBody AuthPositionRequest request);
 
     @Operation(summary = "로그아웃 API", description = "로그아웃 API")
@@ -89,11 +117,19 @@ public interface MemberControllerSwagger {
     })
     ResponseEntity<APISuccessResponse<UserInfoResponse>> getUserInfo();
 
-    @Operation(summary = "사용자 탈퇴 API", description = "사용자 탈퇴 API")
+    @Operation(summary = "사용자 탈퇴 API", description = "사용자 탈퇴 API (소프트 딜리트)")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "사용자 탈퇴 성공"),
-            @ApiResponse(responseCode = "400", description = "사용자 탈퇴 실패", content = @Content(schema = @Schema(implementation = APIErrorResponse.class)))
+            @ApiResponse(responseCode = "202", description = "사용자 탈퇴 성공"),
+            @ApiResponse(responseCode = "400", description = "사용자 탈퇴 실패, 올바르지 않은 탈퇴 코드입니다.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = APIErrorResponse.class),
+                            examples = @ExampleObject(
+                                    name = "올바르지 않은 코드",
+                                    value = "{ \"message\": \"올바르지 않은 탈퇴 코드입니다.\", \"customErrorCode\": \"INVALID_MEMBER_DELETE_CODE\" }"
+                            )
+                    ))
     })
+    @Parameter(name = "Authorization", description = "Bearer {access_token}", in = ParameterIn.HEADER, required = true, schema = @Schema(type = "string"))
     ResponseEntity<APISuccessResponse<Void>> softDeleteMember(@RequestBody MemberDeleteRequest request);
 }
 
--- offroad-api/src/main/java/site/offload/api/member/controller/TokenControllerSwagger.java
+++ offroad-api/src/main/java/site/offload/api/member/controller/TokenControllerSwagger.java
@@ -1,7 +1,10 @@
 package site.offload.api.member.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -9,6 +12,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
 import site.offload.api.member.dto.response.TokenReissueResponse;
+import site.offload.api.response.APIErrorResponse;
 import site.offload.api.response.APISuccessResponse;
 
 @Tag(name = "[Token API] 토큰 관련 API")
@@ -17,8 +21,13 @@ public interface TokenControllerSwagger {
     @Operation(summary = "토큰 재발급 API", description = "Refresh Token으로 Access Token, Refresh Token을 재발급하는 API입니다.")
     @ApiResponses(
             value = {
-                    @ApiResponse(responseCode = "201", description = "토큰 재발급 완료")
+                    @ApiResponse(responseCode = "201", description = "토큰 재발급 완료"),
+                    @ApiResponse(responseCode = "401", description = "토큰 재발급 권한 없음", content = @Content(schema = @Schema(implementation = APIErrorResponse.class),
+                            examples = {
+                                    @ExampleObject(name = "유효하지 않은 리프레시 토큰", value = "{ \"message\": \"유효하지 않은 리프레시 토큰\", \"customErrorCode\": \"FAIL_REISSUE_TOKEN\" }"),
+                            })),
             }
     )
+    @Parameter(name = "Authorization", description = "Bearer {refresh_token}", in = ParameterIn.HEADER, required = true, schema = @Schema(type = "string"))
     ResponseEntity<APISuccessResponse<TokenReissueResponse>> refreshToken(@RequestHeader("Authorization") String tokenHeaderValue);
 }
