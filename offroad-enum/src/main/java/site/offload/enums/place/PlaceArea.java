77155bf1e320610b7c2c10bc578577d26b9548fb
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-09-23 19:22:50 +0900

    chore: 구역 enum에 별칭 추가

--- offroad-enum/src/main/java/site/offload/enums/place/PlaceArea.java
+++ offroad-enum/src/main/java/site/offload/enums/place/PlaceArea.java
@@ -1,13 +1,19 @@
 package site.offload.enums.place;
 
 
-// TODO: 장소 정보를 알 수 있도록, 주석 또는 코드 추가.
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum PlaceArea {
-    AREA1,
-    AREA2,
-    AREA3,
-    AREA4,
-    AREA5,
-    AREA6,
-    NONE
+    AREA1("시간이 머무는 마을"),
+    AREA2("예술가의 거리"),
+    AREA3("트렌드의 시작점"),
+    AREA4("해방의 숲"),
+    AREA5("번영의 도시"),
+    AREA6("지혜의 전당"),
+    NONE("구역 없음");
+
+    private final String placeAreaAlias;
 }
