10eb2635003a9384f7089831dc091c546190cb9c
Kim Uijin <127496156+sansan20535@users.noreply.github.com>
2024-09-23 20:12:28 +0900

    refact : 공지사항 링크 추가 및 공지사항 조회 API 응답 값 수정

--- offroad-api/src/main/java/site/offload/api/announcement/dto/response/AnnouncementDetailResponse.java
+++ offroad-api/src/main/java/site/offload/api/announcement/dto/response/AnnouncementDetailResponse.java
@@ -3,6 +3,7 @@ package site.offload.api.announcement.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record AnnouncementDetailResponse(
         @Schema(description = "공지사항 제목", example = "제목")
@@ -12,9 +13,13 @@ public record AnnouncementDetailResponse(
         @Schema(description = "중요 공지사항 여부", example = "true")
         boolean isImportant,
         @Schema(description = "수정 일자", example = "2024-07-16 21:59:12.000000")
-        LocalDateTime updateAt
+        LocalDateTime updateAt,
+        @Schema(description = "외부 링크 보유 여부", example = "true")
+        boolean hasExternalLinks,
+        @Schema(description = "외부 링크")
+        List<String> externalLinks
 ) {
-    public static AnnouncementDetailResponse of(String title, String content, boolean isImportant, LocalDateTime updateAt) {
-        return new AnnouncementDetailResponse(title, content, isImportant, updateAt);
+    public static AnnouncementDetailResponse of(String title, String content, boolean isImportant, LocalDateTime updateAt, boolean hasExternalLinks, List<String> externalLinks) {
+        return new AnnouncementDetailResponse(title, content, isImportant, updateAt, hasExternalLinks, externalLinks);
     }
 }
