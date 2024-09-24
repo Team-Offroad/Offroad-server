5d2345b011b3480a0f264be981e931277f767d63
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-09-24 14:45:57 +0900

    fix: 장소에 없는 쿠폰사용코드 체크 수정

--- offroad-db/src/main/java/site/offload/db/place/repository/PlaceRepository.java
+++ offroad-db/src/main/java/site/offload/db/place/repository/PlaceRepository.java
@@ -25,4 +25,6 @@ public interface PlaceRepository extends JpaRepository<PlaceEntity, Long> {
             Pageable pageable
     );
 
+    boolean existsByCouponAuthCode(String couponAuthCode);
+
 }
